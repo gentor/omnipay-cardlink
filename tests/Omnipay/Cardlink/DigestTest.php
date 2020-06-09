93244dd63783f5aee5e68675cf99d504dc6eada9
Henryk <henry>
2020-06-09 10:22:28 +0100

    introduce the test containing the greek character

--- tests/Omnipay/Cardlink/DigestTest.php
+++ tests/Omnipay/Cardlink/DigestTest.php
@@ -38,4 +38,35 @@ class DigestTest extends TestCase
         // Try with a "digest" element - should get same answer.
         $this->assertEquals($expectedDigest, DigestCalculator::calculate($data, self::SECRET_KEY));
     }
+
+    public function testDigestWithGreekChar() {
+
+          $data = [
+              'version' => 2,
+              'mid' => '0101119349',
+              'lang' => 'en',
+              'deviceCategory' => '0',
+              'orderid' => 'O170911143656',
+              'orderDesc' => 'UsingGreekCharsΓ', // contains the greek character
+              'orderAmount' => '0.12',
+              'currency' => 'EUR',
+              'payerEmail' => 'cardlink@cardlink.gr',
+              'payerPhone' => '6900000000',
+              'element' => 'GRUK',
+              'payMethod' => 'visa',
+              'confirmUrl' => 'https://euro.test.modirum.com/vpostestsv4/shops/shopdemo.jsp?cmd=confirm',
+              'cancelUrl' => 'https://euro.test.modirum.com/vpostestsv4/shops/shopdemo.jsp?cmd=cancel',
+          ];
+
+        $expectedDigest = 'FUypWRVCh+Q4qdpArKME2OQZlm+anJU4iHwigY6Bp60=';
+
+        // Try without a "digest" element.
+        $this->assertEquals($expectedDigest, DigestCalculator::calculate($data, self::SECRET_KEY));
+
+        $data['digest'] = $expectedDigest;
+
+        // Try with a "digest" element - should get same answer.
+        $this->assertEquals($expectedDigest, DigestCalculator::calculate($data, self::SECRET_KEY));
+
+    }
 }
