a41c1f30687fcf03ce07a4a0a61b7ddb6a37ee89
Henryk <henry>
2020-06-12 15:07:32 +0100

    update the greek character more realistic digest test

--- tests/Omnipay/Cardlink/DigestTest.php
+++ tests/Omnipay/Cardlink/DigestTest.php
@@ -30,42 +30,45 @@ class DigestTest extends TestCase
 
         $expectedDigest = 'Xw19+XA5lQXbzEHvvFYe1Zrm7N+rvpdIvzuIyM9HY3Q=';
 
-        // Try without a "digest" element.
+        // Try without a 'digest' element.
         $this->assertEquals($expectedDigest, DigestCalculator::calculate($data, self::SECRET_KEY));
 
         $data['digest'] = $expectedDigest;
 
-        // Try with a "digest" element - should get same answer.
+        // Try with a 'digest' element - should get same answer.
         $this->assertEquals($expectedDigest, DigestCalculator::calculate($data, self::SECRET_KEY));
     }
 
     public function testDigestWithGreekChar() {
 
           $data = [
-              'version' => 2,
-              'mid' => '0101119349',
+              'version' => '2',
+              'mid' => '0020893996',
               'lang' => 'en',
-              'deviceCategory' => '0',
-              'orderid' => 'O170911143656',
-              'orderDesc' => 'UsingGreekCharsΓ', // contains the greek character
-              'orderAmount' => '0.12',
-              'currency' => 'EUR',
-              'payerEmail' => 'cardlink@cardlink.gr',
-              'payerPhone' => '6900000000',
-              'element' => 'GRUK',
-              'payMethod' => 'visa',
-              'confirmUrl' => 'https://euro.test.modirum.com/vpostestsv4/shops/shopdemo.jsp?cmd=confirm',
-              'cancelUrl' => 'https://euro.test.modirum.com/vpostestsv4/shops/shopdemo.jsp?cmd=cancel',
+              'orderid' => 'DNRX3BJ1x944',
+              'orderDesc' => 'DNRX3BJ1x944',
+              'orderAmount' =>'12.00',
+              'currency' =>'EUR',
+              'payerEmail' => 'henryk.kwak@gmail.com',
+              'payerPhone' =>'',
+              'billCountry' =>'GR',
+              'billState' =>'',
+              'billZip' => '118 52',
+              'billCity' =>'Αθήνα',
+              'billAddress' =>'3',
+              'trType' =>'1',
+              'confirmUrl' =>'http://henryk.digitickets.test/payment-callback/f/DNRX3BJ1x944',
+              'cancelUrl' =>'http://henryk.digitickets.test/payment-callback/f/DNRX3BJ1x944',
           ];
 
-        $expectedDigest = 'FUypWRVCh+Q4qdpArKME2OQZlm+anJU4iHwigY6Bp60=';
+        $expectedDigest = 'THI0toWE2s1zQkKqhLqCbNyyz/Oe2QE2wNSpH6DR8Lo=';
 
-        // Try without a "digest" element.
+        // Try without a 'digest' element.
         $this->assertEquals($expectedDigest, DigestCalculator::calculate($data, self::SECRET_KEY));
 
         $data['digest'] = $expectedDigest;
 
-        // Try with a "digest" element - should get same answer.
+        // Try with a 'digest' element - should get same answer.
         $this->assertEquals($expectedDigest, DigestCalculator::calculate($data, self::SECRET_KEY));
 
     }
